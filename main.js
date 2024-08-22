bf73a8d3b4545913538a2cb13ac00824607fdd28
Hunter-137 <asrarkulovamal@gmail.com>
2024-08-22 15:40:50 +0500

    feat: add practice from lesson №6

--- main.js
+++ main.js
@@ -97,73 +97,159 @@ import * as THREE from "three";
 
 // *****************************************************************************************
 
-// Сцена
-const scene = new THREE.Scene();
-const axesHelper = new THREE.AxesHelper(4);
-scene.add(axesHelper);
+// // Сцена
+// const scene = new THREE.Scene();
+// const axesHelper = new THREE.AxesHelper(4);
+// scene.add(axesHelper);
 
-// Объект
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-  color: "white",
-  wireframe: true,
-});
+// // Объект
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({
+//   color: "white",
+//   wireframe: true,
+// });
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 
-// Камера
-const sizes = {
-  width: 600,
-  height: 600,
-};
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
-scene.add(camera);
+// // Камера
+// const sizes = {
+//   width: 600,
+//   height: 600,
+// };
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// camera.position.z = 3;
+// scene.add(camera);
 
-// Отрисовщик
-const canvas = document.querySelector(".canvas");
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+// // Отрисовщик
+// const canvas = document.querySelector(".canvas");
+// const renderer = new THREE.WebGLRenderer({ canvas });
+// renderer.setSize(sizes.width, sizes.height);
+// renderer.render(scene, camera);
 
-// Ванильная анимация фреймов
-// let time = Date.now();
+// // Ванильная анимация фреймов
+// // let time = Date.now();
+
+// // const tick = () => {
+// //   // console.log(tick);
+
+// //   const currentTime = Date.now();
+// //   const delta = currentTime - time;
+// //   time = currentTime;
+
+// //   // mesh.rotation.y += 0.1 * delta;
+// //   // mesh.rotation.x += 0.1 * delta;
+// //   mesh.rotation.z += 0.1 * delta;
+// //   renderer.render(scene, camera);
+// //   window.requestAnimationFrame(tick);
+// // };
+
+// // tick();
+
+// // Анимация фрейма через three.js
+// const clock = new THREE.Clock();
 
 // const tick = () => {
-//   // console.log(tick);
+//   const elapsedTime = clock.getElapsedTime();
+//   // mesh.rotation.z = elapsedTime * 50;
 
-//   const currentTime = Date.now();
-//   const delta = currentTime - time;
-//   time = currentTime;
+//   // mesh.position.x = Math.cos(elapsedTime);
+//   // mesh.position.y = Math.sin(elapsedTime);
+//   console.log(elapsedTime);
 
-//   // mesh.rotation.y += 0.1 * delta;
-//   // mesh.rotation.x += 0.1 * delta;
-//   mesh.rotation.z += 0.1 * delta;
+//   camera.position.x = Math.cos(elapsedTime);
+//   camera.position.y = Math.sin(elapsedTime);
+//   camera.position.z = Math.sin(elapsedTime - 1);
+
+//   camera.lookAt(mesh.position);
 //   renderer.render(scene, camera);
 //   window.requestAnimationFrame(tick);
 // };
 
 // tick();
 
-// Анимация фрейма через three.js
-const clock = new THREE.Clock();
+// ******************************************************************************************
 
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  // mesh.rotation.z = elapsedTime * 50;
+const scene = new THREE.Scene();
+const canvas = document.querySelector(".canvas");
 
-  // mesh.position.x = Math.cos(elapsedTime);
-  // mesh.position.y = Math.sin(elapsedTime);
-  console.log(elapsedTime);
-
-  camera.position.x = Math.cos(elapsedTime);
-  camera.position.y = Math.sin(elapsedTime);
-  camera.position.z = Math.sin(elapsedTime - 1);
-
-  camera.lookAt(mesh.position);
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(tick);
+const sizes = {
+  width: 600,
+  height: 600,
 };
 
-tick();
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({
+//   color: "yellow",
+//   wireframe: true,
+// });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+const group = new THREE.Group();
+const meshes = [];
+
+const colors = [0xb7e8d8, 0xe86344, 0xe8ab9c];
+
+for (let x = -1.2; x <= 1.2; x += 1.2) {
+  for (let y = -1.2; y <= 1.2; y += 1.2) {
+    const material = new THREE.MeshBasicMaterial({
+      color: colors[Math.floor(Math.random() * 3) + 1],
+      wireframe: true,
+    });
+
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.scale.set(0.5, 0.5, 0.5);
+    mesh.position.set(x, y, 0);
+    meshes.push(mesh);
+  }
+}
+
+group.add(...meshes);
+scene.add(group);
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+const clock = new THREE.Clock();
+const MAX_SCALE = 1;
+const MIN_SCALE = 0.5;
+let grow = false;
+
+const animate = () => {
+  const delta = clock.getDelta();
+
+  meshes.forEach((cube, index) => {
+    const mult = index % 2 === 0 ? 1 : -1;
+
+    cube.rotation.x += mult * delta;
+    cube.rotation.y += mult * delta * 0.4;
+  });
+
+  const elapsed = clock.getElapsedTime();
+  camera.position.x = Math.sin(elapsed);
+  camera.position.y = Math.cos(elapsed);
+
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+  const mult = grow ? 1 : -1;
+  group.scale.x += mult * delta * 0.2;
+  group.scale.y += mult * delta * 0.2;
+  group.scale.z += mult * delta * 0.2;
+
+  if (grow && group.scale.x >= MAX_SCALE) {
+    grow = false;
+  } else if (group.scale.x <= MIN_SCALE) {
+    grow = true;
+  }
+
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animate);
+};
+
+animate();
