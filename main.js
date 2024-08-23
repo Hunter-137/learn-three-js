85e933ef1a5048730c13ca113f25814a9464e3f3
Hunter-137 <asrarkulovamal@gmail.com>
2024-08-23 19:02:18 +0500

    feat: add practice lesson 9

--- main.js
+++ main.js
@@ -257,20 +257,71 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 //  **********************************************************************************************************************
 
+// // сцена
+// const scene = new THREE.Scene();
+// const canvas = document.querySelector(".canvas");
+
+// // камера
+// const sizes = {
+//   width: 600,
+//   height: 600,
+// };
+
+// // const cursor = {
+// //   x: 0,
+// //   y: 0,
+// // };
+
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// camera.position.z = 3;
+// scene.add(camera);
+
+// // для управления объектом
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
+
+// // объект
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({
+//   color: "yellow",
+//   wireframe: true,
+// });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+// // отрисовщик
+// const renderer = new THREE.WebGLRenderer({ canvas });
+// renderer.setSize(sizes.width, sizes.height);
+// renderer.render(scene, camera);
+
+// // window.addEventListener("mousemove", (event) => {
+// //   cursor.x = -(event.clientX / sizes.width - 0.5);
+// //   cursor.y = event.clientY / sizes.height - 0.5;
+// // });
+
+// const tick = () => {
+//   // camera.position.x = cursor.x * 2;
+//   // camera.position.y = cursor.y * 2;
+//   // camera.lookAt(mesh.position);
+
+//   controls.update();
+//   renderer.render(scene, camera);
+//   window.requestAnimationFrame(tick);
+// };
+
+// tick();
+
+//  *********************************************************************************************************************************************************
+
 // сцена
 const scene = new THREE.Scene();
 const canvas = document.querySelector(".canvas");
 
 // камера
 const sizes = {
-  width: 600,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
-
-// const cursor = {
-//   x: 0,
-//   y: 0,
-// };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
@@ -294,19 +345,22 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
-// window.addEventListener("mousemove", (event) => {
-//   cursor.x = -(event.clientX / sizes.width - 0.5);
-//   cursor.y = event.clientY / sizes.height - 0.5;
-// });
-
 const tick = () => {
-  // camera.position.x = cursor.x * 2;
-  // camera.position.y = cursor.y * 2;
-  // camera.lookAt(mesh.position);
-
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
 
 tick();
+
+window.addEventListener("resize", () => {
+  // console.log("Hello, World!");
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.render(scene, camera);
+});
